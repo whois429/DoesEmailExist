f7dc37dafb4d8a2410bff49052183f4dbc3b95d0
429 <117005851+whois429@users.noreply.github.com>
2022-12-03 17:22:20 +0200

    Update script.py

--- script.py
+++ script.py
@@ -3,24 +3,29 @@ from multiprocessing.dummy import Pool
 from validate_email_address import validate_email
 
 
-def get_emails():
-    with open("emails.txt", "r") as file:
-        return [line.rstrip() for line in file]
+COUNT_OF_PROCESSES: int = 4
+EMAILS_FILE_NAME: str = "emails.txt"
 
 
-async def get_request(email):
-    isvalid = validate_email(email, verify=True)
-    if not isvalid:
+def get_emails() -> list[str]:
+    """Gets email addresses from the file"""
+    with open(EMAILS_FILE_NAME, "r") as file:
+        return [line.strip() for line in file]
+
+
+async def check_email(email: str) -> None:
+    """Checks email"""
+    if not validate_email(email, verify=True):
         print(f"{email} not valid!")
 
 
-def wrapper(email):
-    asyncio.run(get_request(email))
+def wrapper(email: str) -> None:
+    """Wrapper to run the 'check_email' function asynchronously"""
+    asyncio.run(check_email(email))
 
 
 if __name__ == "__main__":
     emails = get_emails()
-    emails.append("somemfks3434fdsf3@rambler.ru")
 
-    with Pool(processes=4) as executor:
+    with Pool(processes=COUNT_OF_PROCESSES) as executor:
         executor.map(wrapper, emails)
